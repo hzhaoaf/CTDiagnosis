3bd4e98e470a818f5f4b412f743a6a84c8a68046
wangjizhe <buptjz@gmail.com>
2013-09-05 16:56:12 +0800

    finish greycomatrix which is the pain in the ass

--- Curvelet/ccf.py
+++ Curvelet/ccf.py
@@ -34,15 +34,52 @@
 from scipy import misc
 import matplotlib.pyplot as plt
 from fdct_wrapping import *
+from skimage.feature import greycomatrix
+from preglcm import preglcm
 
 def ccf( x, graylevel=16, is_real = 1, finest = 2, nbscales = 3, nbangles_coarse = 32):
-    #C = [[],[[],[],[]],[]]---->C[0][0].shape is (33, 45),
-    #C[1] is [(27, 24),(25,23),(25,23),(25,24)
+    #% curvelet decomposition
+    #%   C           Cell array of curvelet coefficients.
+    #%               C{j}{l}(k1,k2) is the coefficient at
+    #%                   - scale j: integer, from finest to coarsest scale,
+    #%                   - angle l: integer, starts at the top-left corner and
+    #%                   increases clockwise,
+    #%                   - position k1,k2: both integers, size varies with j
+    #%                   and l.
+    #%               If is_real is 1, there are two types of curvelets,
+    #%               'cosine' and 'sine'. For a given scale j, the 'cosine'
+    #%               coefficients are stored in the first two quadrants (low
+    #%               values of l), the 'sine' coefficients in the last two
+    #%               quadrants (high values of l).      
     C = fdct_wrapping(x, is_real, finest, nbscales, nbangles_coarse)
+    ANGLES = numpy.zeros((1,len(C)))
+    for i in range(len(C)):
+        ANGLES[0,i] = len(C[i]) #array([[  1.,  32.,   1.]])
+        
+    #% offsets of 4 directions of co-occurrence matrix: 0, 45, 90, 135 degree
+    offset = numpy.array([[0,1],[-1,1],[-1,0],[-1,-1]])
+    
+    index = 1
+    #% for every scale
+    
+    for i in range(len(C)):
+        #% for every direction of curvelet in this scale
+        for j in range(len(C[i])):
+            #% calculate average co-occurrence matrix
+            SI = preglcm(C[i][j],graylevel)
+            #greycomatrix implemented by skimage ranges from 0~15,but SI ranges from 1~16
+            #offset = [ 0 1;-1 1;-1 0;-1 -1 ];And the greycomatrix is weird,I tried it so many times.
+            glcms = greycomatrix(SI - 1,[1],[0,numpy.pi*3/4,numpy.pi/4] ,levels=graylevel, symmetric=False, normed=False)
+            glcm1 = glcms[:,:,0,0]
+            glcm3 = glcms[:,:,0,1]
+            glcm4 = glcms[:,:,0,2]
+            glcm2 = greycomatrix(numpy.fliplr(SI) - 1,[1],[numpy.pi/4] ,levels=graylevel, symmetric=False, normed=False)[:,:,0,0]
+
+            pass
     return C
 
 if __name__ == "__main__":
-    XX = misc.imread('testa.jpg')
+    XX = misc.imread('test1.jpg')
     XX= XX[:,:,0] if XX.shape[2] > 1 else XX#if RGB,only tackle R
     
     ccf(XX)
---
+++ Curvelet/preglcm.py
@@ -0,0 +1,14 @@
+
+import numpy
+
+#preprocess with GRAYCOMATRIX Create gray-level co-occurrence matrix.
+#WangJZ 2013.9.5
+
+def preglcm(I,NL):
+        #% Scale I so that it contains integers between 1 and NL.
+        minN = I.min()
+        maxN = I.max()
+        slope = (NL-1) / (maxN- minN)
+        intercept = 1 - (slope*(minN))
+        SI = numpy.round(slope * I + intercept).astype(numpy.int)
+        return SI
