59d964d56cf435e48b03c9241e04ed88b685de01
wangjizhe <buptjz@gmail.com>
2013-09-27 09:55:39 +0800

    modify

--- GUI/ImageViewPrivate.py
+++ GUI/ImageViewPrivate.py
@@ -1,14 +1,14 @@
 from PyQt4 import QtGui, QtCore
+from PyQt4.QtCore import Qt
+
 from ImageViewPrivate import ImageViewPrivate
 
 class ImageViewPrivate(QtGui.QWidget):
 	def __init__(self,_p):
 		self.setPalette(QtGui.QPalette(QtGui.QColor(128,128,128)))
-		self.
-  setAttribute(Qt::WA_NoSystemBackground);
-  setAttribute(Qt::WA_OpaquePaintEvent);
-  p=_p;
-  rb=NULL;
-  moveRb=false;
-  setMouseTracking(true);
- }
+		self.setAttribute(Qt.WA_NoSystemBackground)
+		self.setAttribute(Qt.WA_OpaquePaintEvent)
+		self.p = _p
+		self.rb = None
+		self.moveRb = False
+		self.setMouseTracking(True)
